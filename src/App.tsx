c7ad35e2c5221bbd7ba51372edaa5dcf751e21a8
DimasBrahmantya <dimasbrahmantyaaryasena@gmail.com>
2026-01-08 19:29:52 +0700

    fixing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import "./styles/globals.css";
-//import { Toaster } from "./components/ui/sonner";
-//import { createRoot } from "react-dom/client";
 import { Toaster as Sonner } from "./components/ui/sonner";
 import { TooltipProvider } from "./components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-//import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
 import { Login } from "./pages/Login/Login";
 import TwoFactorAuth from "./pages/2FA/TwoFactorAuth";
 import NotFound from "./pages/NotFound";
